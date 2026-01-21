420848fd9615a44080cd4465608c78219634eb64
Soteris24 <66694845+Soteris24@users.noreply.github.com>
2026-01-21 10:20:22 +0200

    Add movement forward

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -4,15 +4,30 @@ import rclpy
 from rclpy.node import Node
 
 # Fill in something for msg type imports
-# from duckietown_msgs.msg import SOMETHING
-# from std_msgs.msg import SOMETHING
+from duckietown_msgs.msg import WheelsCmdStamped
 
 class SkeletonNode(Node):
     def __init__(self):
         super().__init__('blank_node')
         #Create publishers and subscribers in init, use callback
-        pass
+        self.vehicle_name = os.getenv('VEHICLE_NAME')
+        self.wheel_pub = self.create_publisher(WheelsCmdStamped, f'/{self.vehicle_name}/wheels_cmd', 10)
+        self.timer = self.create_timer(0.1, self.move_forward_callback)
+        self.get_logger().info('Forward')
     
+
+    def move_forward_callback(self):
+        # Create the wheel command message
+        msg = WheelsCmdStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        
+
+        msg.vel_left = 0.3  
+        msg.vel_right = 0.3
+        
+        # Publish
+        self.wheel_pub.publish(msg)
+        self.get_logger().info(f'Publishing: left={msg.vel_left}, right={msg.vel_right}')
     #Define callback functions here
 
 
@@ -20,8 +35,14 @@ def main():
     print('In main')
     rclpy.init()
     node = SkeletonNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
