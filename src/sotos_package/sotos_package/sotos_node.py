d88961cd9c1d0635a1904b141dff5cb4a65b2c15
Soteris24 <66694845+Soteris24@users.noreply.github.com>
2026-01-23 11:24:45 +0200

    ONLY A TEST

--- src/sotos_package/sotos_package/sotos_node.py
+++ src/sotos_package/sotos_package/sotos_node.py
@@ -38,13 +38,13 @@ class EncoderAvoidanceNode(Node):
         # Config
         self.TURN_TICKS = 100 # Ticks to turn 
         self.DRIVE_TICKS = 400 # Ticks to drive forward
-        self.NORMAL_SPEED = 10 # Speed in normal mode
+        self.NORMAL_SPEED = 5 # Speed in normal mode
         self.TOLERANCE = 3 # Arrival tolerance
         
         
         self.MAX_SPEED = 0.5    
 
-        self.create_timer(0.05, self.control_loop)
+        self.create_timer(0.02, self.control_loop)
 
     def control_loop(self):
         if not self.initialized: return
@@ -59,6 +59,10 @@ class EncoderAvoidanceNode(Node):
             self.set_leds(1, 0, 0) # Red
             if self.has_arrived():
                 self.get_logger().info("Moving Forward")
+                if self.state == State.NORMAL and msg.range < 0.2 and msg.range > 0.01:
+                    self.get_logger().info("Turning Left...")
+                    self.state = State.AVOID_TURN_OUT
+                
                 self.state = State.AVOID_DRIVE_PAST
                 
                 self.target_left = self.left_ticks + self.DRIVE_TICKS
